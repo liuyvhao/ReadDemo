047f1e1e15d845248380d9fe0e859df2d6479033
liuyvhao <yvhao_liu@163.com>
2018-08-15 15:43:14 +0800

    完成textview显示动态图片

--- app/src/main/java/lyh/readdemo/BookHotActivity.kt
+++ app/src/main/java/lyh/readdemo/BookHotActivity.kt
@@ -2,6 +2,10 @@ package lyh.readdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_book_hot.*
+import lyh.util.Head
 
 /**
  * 热门
@@ -11,5 +15,19 @@ class BookHotActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_hot)
+        back.setOnClickListener {
+            webView.goBack()
+        }
+        var websetting = webView.settings
+        websetting.javaScriptEnabled = true
+        websetting.allowFileAccess = true
+        websetting.builtInZoomControls = true
+        webView.loadUrl(Head.urlHead)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+        }
     }
 }
